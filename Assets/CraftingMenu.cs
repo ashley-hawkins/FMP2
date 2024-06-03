45b35ccccf7c798b2f72815a2be372aadb572327
Ashley Hawkins <536669@escg.ac.uk>
2024-06-03 12:15:38 +0100

    Start adding crafting menu code

---
+++ Assets/CraftingMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FMP
+{
+    public class CraftingMenu : MonoBehaviour
+    {
+        List<ItemBase> craftables;
+
+        List<ItemStack> GetCraftableItems(List<ItemStack> availableResources)
+        {
+            List<ItemBase> allItems = new(); // placeholder
+            foreach (ItemBase item in allItems)
+            {
+                
+            }
+        }
+
+        void Refresh()
+        {
+
+        }
+    }
+}
